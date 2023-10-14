e873490d98e686cfba279d4e8a6f1f33bde5aa84
yogyy <yogimr261@gmail.com>
2023-10-14 17:22:58 +0700

    feat: add dark mode

---
+++ src/hooks/use-theme.tsx
@@ -0,0 +1,13 @@
+import { ThemeProviderContext } from '@/components/theme-provider';
+import { useContext } from 'react';
+
+const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+
+  if (context === undefined)
+    throw new Error('useTheme must be used within a ThemeProvider');
+
+  return context;
+};
+
+export default useTheme;

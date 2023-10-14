dc0a49e87aba68c9127591005bac781589b36558
yogyy <yogimr261@gmail.com>
2023-10-14 17:23:53 +0700

    faet: add new icons
    rename some file

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,6 +1,10 @@
 export type SvgProps = React.SVGAttributes<SVGElement>;
 
-
-export { default as LoaderIcon } from './Loader';
-export { default as StarIcon } from './Star';
-export { default as ChevronDownIcon } from './ChevronDown';
+export { default as Loader } from './loader';
+export { default as Star } from './star';
+export { default as ChevronDown } from './chevron-down';
+export { default as Sun } from './sun';
+export { default as Moon } from './moon';
+export { default as Check } from './check';
+export { default as ChevronRight } from './chevron-right';
+export { default as Circle } from './circle';
